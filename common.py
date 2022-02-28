6f269210ae37a13dd11036919d1b25bbafe590d7
makiprogrammer <63451795+makiprogrammer@users.noreply.github.com>
2022-02-28 16:20:02 +0100

    Revert "After-version - 2nd attempt and self-evaluator"
    This reverts commit c74e42305ff52baeef30e78baf3578da91a93818.

--- common.py
+++ common.py
@@ -1,76 +1,70 @@
 
 # CLASSES
 class Contributor:
-	def __init__(self, name: str, skills: dict[str, int]):
-		self.name = name
-		self.skills = skills
+  def __init__(self, name: str, skills: dict[str, int]):
+    self.name = name
+    self.skills = skills
+
+    self.available: bool = True
 
 class Project:
-	def __init__(self, name: str, days: int, score: int, deadline: int, roles: list[tuple[str, int]]):
-		self.name = name
-		self.days = days
-		self.score = score
-		self.deadline = deadline
-		self.roles = roles
+  def __init__(self, name: str, days: int, score: int, deadline: int, roles: dict[str, int]):
+    self.name = name
+    self.days = days
+    self.score = score
+    self.deadline = deadline
+    self.roles = roles
 
-		self.contributors: list[Contributor] = []
-		self.start_day: int = -1
-
-		self.role_indexes: dict[tuple[str, int], list[int]] = {}
-		for role in self.roles:
-			self.role_indexes[role] = []
-		for i, role in enumerate(self.roles):
-			self.role_indexes[role].append(i)
-	
+    self.contributors: dict[str, Contributor] = {}
+    self.start_day: int = -1
 
 # PARSER
 def parse(filename) -> tuple[list[Contributor], list[Project]]:
-	with open(filename) as f:
-		lines = f.readlines()
+  with open(filename) as f:
+    lines = f.readlines()
 
-	total_contributors, total_projects = tuple(map(int, lines[0].split()))
-	projects = []
-	contributors = []
+  total_contributors, total_projects = tuple(map(int, lines[0].split()))
+  projects = []
+  contributors = []
 
-	current_line = 1
+  current_line = 1
 
-	# contributors
-	for i in range(total_contributors):
-		name = lines[current_line].split()[0]
-		number_of_skills = int(lines[current_line].split()[1])
+  # contributors
+  for i in range(total_contributors):
+    name = lines[current_line].split()[0]
+    number_of_skills = int(lines[current_line].split()[1])
 
-		contributor = Contributor(name, {})
-		contributors.append(contributor)
+    contributor = Contributor(name, {})
+    contributors.append(contributor)
 
-		# contributor's skills
-		current_line += 1
-		for j in range(number_of_skills):
-			skill_name = lines[current_line].split()[0]
-			skill_level = int(lines[current_line].split()[1])
-			contributor.skills[skill_name] = skill_level
+    # contributor's skills
+    current_line += 1
+    for j in range(number_of_skills):
+      skill_name = lines[current_line].split()[0]
+      skill_level = int(lines[current_line].split()[1])
+      contributor.skills[skill_name] = skill_level
 
-			current_line += 1
+      current_line += 1
 
-	# projects
-	for i in range(total_projects):
-		name = lines[current_line].split()[0]
-		days, score, deadline, project_roles = tuple(
-				map(int, lines[current_line].split()[1:]))
-		current_line += 1
+  # projects
+  for i in range(total_projects):
+    name = lines[current_line].split()[0]
+    days, score, deadline, project_roles = tuple(
+        map(int, lines[current_line].split()[1:]))
 
-		# project's roles
-		roles: list[tuple[str, int]] = []
-		for j in range(project_roles):
-			role_name = lines[current_line].split()[0]
-			role_min_level = int(lines[current_line].split()[1])
-			roles.append((role_name, role_min_level))
+    project = Project(name, days, score, deadline, {})
+    projects.append(project)
 
-			current_line += 1
+    # project's roles
+    current_line += 1
+    for j in range(project_roles):
+      role_name = lines[current_line].split()[0]
+      role_min_level = int(lines[current_line].split()[1])
+      project.roles[role_name] = role_min_level
 
-		project = Project(name, days, score, deadline, roles)
-		projects.append(project)
+      current_line += 1
 
-	return contributors, projects
+  return contributors, projects
 
 
 # # just to test the parser
--- main.py
+++ main.py
@@ -1,54 +1,155 @@
-import json
-from common import Contributor, Project, parse
-from stats import get_function_execution_times, get_function_usage_counts, reset_stats
-from solve import solve_single_set
+from common import parse, Contributor, Project
 
-from evaluate import evaluate_single_set
+def get_all_skills(contributors: list[Contributor]) -> set[str]:
+  skills = set()
+  for c in contributors:
+    skills.update(c.skills.keys())
+  return skills
+
+def least_experienced_person(people: set[Contributor]) -> Contributor:
+  # if len(people) == 0: return None
+  # # if len(people) < 100:
+  # #   return min(people, key=avg_contributor_skill_level)
+  for p in people:
+    if p.available:
+      return p
+  return None
+
+def find_people_to_project(project, skill_level_contributors):
+  people: dict[str, Contributor] = {}
+
+  for skill, minimum_skill_required in project.roles.items():
+    available_skill_levels = skill_level_contributors[skill].keys()
+    for level in available_skill_levels:
+      if minimum_skill_required <= level and len(skill_level_contributors[skill][level]) > 0:
+        person = least_experienced_person(skill_level_contributors[skill][level])
+        if person is None: break
+        people[skill] = person
+        person.available = False
+    
+    if skill not in people:
+      # reset - attempt failed
+      for person in people.values():
+        person.available = True
+      return None # this happens too frequently, i guess
+
+  return people
+
+def avg_project_skill_level(project: Project) -> float:
+  return sum(project.roles.values()) / len(project.roles)
+
+def avg_contributor_skill_level(contributor: Contributor) -> float:
+  return sum(contributor.skills.values()) / len(contributor.skills)
+
+def avg_contributors_skill_level(contributors: list[Contributor]) -> float:
+  total_contributor_skills = 0
+  total_contributor_skills_count = 0
+
+  for c in contributors:
+    total_contributor_skills += sum(c.skills.values())
+    total_contributor_skills_count += len(c.skills)
+  return total_contributor_skills / total_contributor_skills_count
+
+def sort_projects(projects: list[Project]) -> list[Project]:
+  # return sorted(projects, key=avg_project_skill_level)
+  # return sorted(projects, key=deadline)
+  return sorted(projects, key=lambda project: project.score/project.days, reverse=True)
+
+def main(contributors: list[Contributor], projects: list[Project]):
+  
+  current_day = 0
+  max_day = 2*max(map(lambda p: p.days, projects))
+
+  available_projects: set[Project] = set(projects)
+  ongoing_projects: set[Project] = set()
+  projects_in_order: list[Project] = [] # completed projects
+
+  available_people = len(contributors)
+  sorted_projects = sort_projects(list(available_projects))
+
+  skill_level_contributors: dict[str, dict[int, set[Contributor]]] = {}
+  for skill in get_all_skills(contributors):
+    skill_level_contributors[skill] = {}
+  for c in contributors:
+    for skill in c.skills:
+      level = c.skills[skill]
+      if level not in skill_level_contributors[skill]:
+        skill_level_contributors[skill][level] = set()
+      skill_level_contributors[skill][level].add(c)
+
+  while current_day < max_day and len(available_projects) > 0:
+
+    # complete ongoing projects
+    for project in ongoing_projects.copy():
+      if project.start_day + project.days != current_day:
+        continue
+      
+      ongoing_projects.remove(project)
+      
+      # free up people
+      available_people += len(project.contributors)
+      for person in project.contributors.values():
+        person.available = True
+        for skill, level in person.skills.items():
+          skill_level_contributors[skill][level].add(person)
+
+    # start new projects
+    for project in sorted_projects:
+      if available_people < len(project.roles):
+        continue
+
+      people = find_people_to_project(project, skill_level_contributors)
+      if people is None: continue
+      if len(people) != len(project.roles):
+        print("ERROR: something went wrong")
+        # raise Exception("something went wrong")
+
+      # now we assume that this project is performed
+      # remove people from skill_level_contributors
+      for person in people.values():
+        for skill, level in person.skills.items():
+          skill_level_contributors[skill][level].remove(person)
+      available_people -= len(people)
+
+      project.contributors = people
+      project.start_day = current_day
+      
+      available_projects.remove(project)
+      ongoing_projects.add(project)
+      projects_in_order.append(project)
+
+      # re-sort sorted_projects
+      sorted_projects = sort_projects(list(available_projects))
+
+    current_day += 1
+    print(max_day, current_day, len(available_projects), len(ongoing_projects))
+
+  return projects_in_order
 
 filenames = [
-	"files/a_an_example.in.txt",
-	"files/b_better_start_small.in.txt",
-	"files/c_collaboration.in.txt",
-	"files/d_dense_schedule.in.txt",
-	"files/e_exceptional_skills.in.txt",
-	"files/f_find_great_mentors.in.txt",
+  "files/a_an_example.in.txt",
+  "files/b_better_start_small.in.txt",
+  "files/c_collaboration.in.txt",
+  "files/d_dense_schedule.in.txt",
+  "files/e_exceptional_skills.in.txt",
+  "files/f_find_great_mentors.in.txt",
 ]
 
-total_score = 0
-
 for filename in filenames:
-	contributors, projects = parse(filename)
+  contributors, projects = parse(filename)
 
-	possible_max_score = sum(project.score for project in projects)
-	print(f"For file {filename} is possible max score = {possible_max_score}")
+  possible_max_score = sum(project.score for project in projects)
+  print(f"For file {filename} id possible max score = {possible_max_score}")
 
-	# solve this set
-	print('Solving...', filename)
-	projects_in_order = solve_single_set(contributors, projects)
+  projects_in_order = main(contributors, projects)
+  with open(filename.replace("in", "out"), "w") as f:
+    f.write(str(len(projects_in_order)))
+    f.write("\n")
+    for project in projects_in_order:
+      f.write(f'{project.name}\n')
+      for role in project.roles:
+        f.write(project.contributors[role].name)
+        f.write(" ")
+      f.write("\n")
 
-	# write the output
-	output_file = filename.replace("in", "out")
-	with open(output_file, "w") as f:
-		f.write(str(len(projects_in_order)) + '\n')
-		for project in projects_in_order:
-			f.write(project.name + '\n')
-			f.write(' '.join(map(lambda person: person.name, project.contributors)) + '\n')
-
-	# save statistics
-	json_filename = filename.replace(".txt", ".json")
-	with open(json_filename.replace("in", "times"), "w") as f:
-		f.write(json.dumps(get_function_execution_times(), indent='\t'))
-	with open(json_filename.replace("in", "usage"), "w") as f:
-		f.write(json.dumps(get_function_usage_counts(), indent='\t'))
-	reset_stats()
-
-	# evaluation of results
-	print("Evaluating...", output_file)
-	score_from_this_set = evaluate_single_set(filename, output_file)
-	if score_from_this_set:
-		total_score += score_from_this_set
-		print("Score", score_from_this_set)
-
-print("Total score", total_score)
-
-	
+  
